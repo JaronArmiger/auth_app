435588ef2197258f71cf38ea0e5e6f96faefcf6f
JaronArmiger <jaron.armiger@gmail.com>
2020-08-26 15:21:56 -0500

    move user model definition into models/user.js

--- app.js
+++ app.js
@@ -5,22 +5,14 @@ const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
+const mongoose = require('mongoose');
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useUnifiedTopology: true,
   useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
 
-const User = mongoose.model(
-  'User',
-  new Schema({
-  	username: { type: String, required: true },
-  	password: { type: String, required: true }
-  })
-);
 
 const app = express();
 app.set('views', __dirname);
